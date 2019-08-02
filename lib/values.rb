1320f97a8e95d63481dd6ba3ac06e9c28aa09457
Renee Sims <info@reneesoul.com>
2019-08-02 11:06:21 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,7 +11,7 @@ require 'pry'
 def get_the_values(groceries)
   values = groceries.values.flatten |values|
 # binding.pry
-values.sort
+  values.sort
   return values.first
   # return groceries.values.first
 end
